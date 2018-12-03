41f7601032869c67d05b396371e02b949b7dd04a
BillioJeverson <billio_jeverson@mymail.sutd.edu.sg>
2018-12-03 10:59:35 +0800

    VolleyUploadApp
    //the most impt part is the endpoints class to specify the endpoints_URL
    
    //The mainactivity class has the UploadBitMap class that specify the key and process the upload

---
+++ VolleyUpload/app/src/main/java/com/example/billio/volleyupload/EndPoints.java
@@ -0,0 +1,6 @@
+package com.example.billio.volleyupload;
+
+public class EndPoints {
+    private static final String ROOT_URL = "https://chocolatepie.tech/admin/testing";
+    public static final String UPLOAD_URL = ROOT_URL ;
+}
