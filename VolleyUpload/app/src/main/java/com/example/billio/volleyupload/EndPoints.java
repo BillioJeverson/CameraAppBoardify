57edca795e1a8067aac7efbf44cf76f693884d8e
BillioJeverson <billio_jeverson@mymail.sutd.edu.sg>
2019-05-23 23:03:24 +0800

    Added Volley

--- VolleyUpload/app/src/main/java/com/example/billio/volleyupload/EndPoints.java
+++
@@ -1,6 +0,0 @@
-package com.example.billio.volleyupload;
-
-public class EndPoints {
-    private static final String ROOT_URL = "https://chocolatepie.tech/admin/testing";
-    public static final String UPLOAD_URL = ROOT_URL ;
-}
